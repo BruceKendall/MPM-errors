a0c8822c4e26597a17be3199b6c2e8fb8f23ccd7
Bruce Kendall <kendall@bren.ucsb.edu>
2018-05-07 08:31:23 -0700

    Generate full source info for test data

---
+++ src/gen_test_data.R
@@ -0,0 +1,14 @@
+### Generate the citation info for the test data, to be put into the new spreadsheet format for the compadrinos
+
+library(ProjectTemplate)
+load.project()
+
+# The subsetting is needed to convert the data frame back to a character vector
+doi_list <- read.csv("reports/doi_list.csv", stringsAsFactors = FALSE, 
+                     colClasses = "character")[[1]]
+
+focal_entries <- match(doi_list, comadre$metadata$DOI.ISBN)
+focal_db <- comadre$metadata[focal_entries, ]
+
+output_fields <- c("Authors", "Journal", "YearPublication", "DOI.ISBN", "AdditionalSource")
+write.csv(focal_db[output_fields], "reports/test_src.csv", quote = FALSE, fileEncoding = "UTF-8")

6c703edc6854d2fbe5247e133a5fa4c6aa004564
Bruce Kendall <kendall@bren.ucsb.edu>
2018-05-07 09:13:34 -0700

    Clean up to get proper format of input and output

--- src/gen_test_data.R
+++ src/gen_test_data.R
@@ -5,10 +5,11 @@ load.project()
 
 # The subsetting is needed to convert the data frame back to a character vector
 doi_list <- read.csv("reports/doi_list.csv", stringsAsFactors = FALSE, 
-                     colClasses = "character")[[1]]
+                     colClasses = "character", header = FALSE)[[1]]
 
 focal_entries <- match(doi_list, comadre$metadata$DOI.ISBN)
 focal_db <- comadre$metadata[focal_entries, ]
 
 output_fields <- c("Authors", "Journal", "YearPublication", "DOI.ISBN", "AdditionalSource")
-write.csv(focal_db[output_fields], "reports/test_src.csv", quote = FALSE, fileEncoding = "UTF-8")
+write.csv(focal_db[output_fields], "reports/test_src.csv", quote = FALSE, 
+          fileEncoding = "UTF-8", row.names = FALSE)
